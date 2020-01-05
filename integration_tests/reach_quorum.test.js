fc4a0ea87623f0d74a4e406f5c09171f2c84603a
Reuven Podmazo <reuven@enigma.co>
2020-01-05 16:46:25 +0200

    refactored the reach_quorum test and added an explicit timeout to one of the `it`s

--- integration_tests/reach_quorum.test.js
+++ integration_tests/reach_quorum.test.js
@@ -10,12 +10,21 @@ const {Store, configureWeb3Account} = require("@salad/operator");
 
 const {DEALS_COLLECTION, DEPOSITS_COLLECTION, CACHE_COLLECTION} = require('@salad/operator/src/store');
 
+const MIX_AMOUNT = Web3.utils.toWei('0.01');
+
+// all the values passed here must be positive, and the ratio must be in [0,1) or else this is wrong.
+function within(ratio_num, ratio_denom, target, value) {
+    const high = target.muln(ratio_denom + ratio_num).divn(ratio_denom);
+    const low = target.muln(ratio_denom - ratio_num).divn(ratio_denom);
+    // console.log([high, diff, low, value].map(x => x.toString() / 1e+18));
+    return value.lte(high) && value.gte(low);
+}
+
 describe('Salad', () => {
     let salad1;
     let salad2;
     let salad3;
     let opts;
-    let web3Utils;
     let balances;
     let sender1;
     let recipient1;
@@ -27,7 +36,6 @@ describe('Salad', () => {
     const provider = new Web3.providers.HttpProvider(`http://${process.env.ETH_HOST}:${process.env.ETH_PORT}`);
     const operatorUrl = `ws://${process.env.OPERATOR_HOST}:${process.env.WS_PORT}`;
     const web3 = new Web3(provider);
-    web3Utils = web3.utils;
 
     before(async () => {
         await configureWeb3Account(web3);
@@ -41,17 +49,13 @@ describe('Salad', () => {
     });
 
     async function makeDeposit(salad, sender, recipient, amount) {
-
-        let encRecipient;
-        let signature;
-
         const receipt = await salad.makeDepositAsync(sender, amount, opts);
         expect(receipt.status).to.equal(true);
-        encRecipient = await salad.encryptRecipientAsync(recipient);
+        const encRecipient = await salad.encryptRecipientAsync(recipient);
         const pubKey = salad.keyPair.publicKey;
 
-        signature = await salad.signDepositMetadataAsync(sender, amount, encRecipient, pubKey);
-        const sigBytes = web3Utils.hexToBytes(signature);
+        const signature = await salad.signDepositMetadataAsync(sender, amount, encRecipient, pubKey);
+        const sigBytes = Web3.utils.hexToBytes(signature);
         expect(sigBytes.length).to.equal(65);
 
         const result = await salad.submitDepositMetadataAsync(sender, amount, encRecipient, pubKey, signature);
@@ -59,13 +63,13 @@ describe('Salad', () => {
     }
 
     async function getBalances(symbol) {
-        for (let account of [sender1, recipient1, sender2, recipient2, sender3, recipient3]) {
+        for (const account of [sender1, recipient1, sender2, recipient2, sender3, recipient3]) {
             let accountBalances = balances.get(account);
             if (accountBalances === undefined) {
                 accountBalances = new Map();
                 balances.set(account, accountBalances);
             }
-            accountBalances.set(symbol, await web3.eth.getBalance(account, 'latest'));
+            accountBalances.set(symbol, Web3.utils.toBN(await web3.eth.getBalance(account, 'latest')));
         }
     }
 
@@ -84,25 +88,32 @@ describe('Salad', () => {
     });
 
     it('make deposit', async () => {
-        let amount = web3Utils.toWei("0.01");
-        await makeDeposit(salad1, sender1, recipient1, amount);
-        await makeDeposit(salad2, sender2, recipient2, amount);
-        await makeDeposit(salad3, sender3, recipient3, amount);
+        await makeDeposit(salad1, sender1, recipient1, MIX_AMOUNT);
+        await makeDeposit(salad2, sender2, recipient2, MIX_AMOUNT);
+        await makeDeposit(salad3, sender3, recipient3, MIX_AMOUNT);
     });
 
-    it('should listen for balances to change', async () => {
-        do {
-            await getBalances('after');
-            await utils.sleep(3000);
-        } while (
-            balances.get(recipient1).get('before') === balances.get(recipient1).get('after')
-            || balances.get(recipient2).get('before') === balances.get(recipient2).get('after'))
+    it('should send funds and trigger the DEAL_EXECUTED_UPDATE event', async () => {
+        const deal = await new Promise(resolve => salad1.onDealExecuted(resolve));
+        console.log('the deal was:', deal);
 
-        let accountDiff = new Map();
-        for (let [account, accountBalances] of balances.entries()) {
-            accountDiff.set(account, (accountBalances.get('after') - accountBalances.get('before')) / 10**18);
+        await getBalances('after');
+        const accountDiff = new Map();
+        for (const [account, accountBalances] of balances.entries()) {
+            accountDiff.set(account, accountBalances.get('after').sub(accountBalances.get('before')).toString() / 1e+18);
         }
-        console.log('The accounts have had their balances changed as follows:');
-        console.log(accountDiff);
-    });
+        console.log('The accounts have had their balances changed as follows:', accountDiff);
+
+        // Expect all recipients to have had their balances increase within a certain proportion of the mix amount.
+        expect([recipient1, recipient2, recipient3]
+            .map(recipient => balances.get(recipient))
+            .every(balance => within(
+                15,
+                100,
+                Web3.utils.toBN('1'+'0'.repeat(16)),
+                balance.get('after').sub(balance.get('before')),
+            ))
+        ).to.equal(true);
+
+    }).timeout(120 * 1000);
 });
