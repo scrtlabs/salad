99d69cafef14cf8b7ba610224f8c147c02a06735
Moshe Demri <mdemri@gmail.com>
2020-01-09 16:35:43 +0200

    cosmetics

--- make-enigma-js.sh
+++ make-enigma-js.sh
@@ -2,16 +2,16 @@ set -e
 
 rm -rf temp
 mkdir temp
-pushd temp
+cd temp
 git clone https://github.com/enigmampc/enigma-contract.git
-pushd enigma-contract
+cd enigma-contract
 yarn
 # Assumption truffle-cli exists
 truffle compile
 cd enigma-js
 yarn
 yarn webpack --env build
-popd
-popd
+cd ..
+cd ..
 
 
